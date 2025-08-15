e41bfe229025b88fd43e4f7c50eec4f0c50b25e3
Rodrigo Leon <yoyiyomeco@hotmail.com>
2025-08-15 00:39:51 -0700

    update: Upgrade onboarding and fix android problem

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
+    host: host || "0.0.0.0", // Permite conexiones desde cualquier IP en la red local
     hmr: host
       ? {
           protocol: "ws",
