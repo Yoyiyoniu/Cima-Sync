ca7a20e67ed418b135c56533974c450b64826702
Yoyiyoniu <yoyiyomeco@hotmail.com>
2026-06-19 00:02:53 -0700

    fix: remove ios_path from plugins without iOS native code

--- plugins/tauri-plugin-android-services/build.rs
+++ plugins/tauri-plugin-android-services/build.rs
@@ -3,6 +3,5 @@ const COMMANDS: &[&str] = &["ping", "start_service", "stop_service", "is_running
 fn main() {
   tauri_plugin::Builder::new(COMMANDS)
     .android_path("android")
-    .ios_path("ios")
     .build();
 }
